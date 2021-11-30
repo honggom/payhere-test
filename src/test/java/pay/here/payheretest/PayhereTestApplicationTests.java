5225595f0ad2863f909953677f8db649ae742953
honggom <cavok699@naver.com>
2021-11-30 15:43:30 +0900

    init: Init

---
+++ src/test/java/pay/here/payheretest/PayhereTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pay.here.payheretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayhereTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
